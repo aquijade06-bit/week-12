732f145e51d9e9187acf5a8d0e5ac44183d573fa
Grace <aquijade06@gmailcom>
2025-12-02 22:41:58 +0700

    Store object to person.ser

--- src/week12_134176/Person.java
+++ src/week12_134176/Person.java
@@ -1,5 +1,46 @@
 package week12_134176;
 
-public class Person {
+import java.io.Serializable;
 
+public class Person implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+    private int age;
+    private String campus;
+
+    public Person(String name, int age, String campus) {
+        this.setName(name);
+        this.setAge(age);
+        this.setCampus(campus);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCampus() {
+        return campus;
+    }
+
+    public void setCampus(String campus) {
+        this.campus = campus;
+    }
+
+   
+    public String toString() {
+        return "Person{name='" + getName() + "', age=" + getAge() + ", campus='" + getCampus() + "'}";
+    }
 }
