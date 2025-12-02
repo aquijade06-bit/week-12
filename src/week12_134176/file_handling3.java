718969991434a381ca8b91d436145b908eea4a1d
Grace <aquijade06@gmailcom>
2025-12-02 22:31:16 +0700

    Write to myfile.txt

--- src/week12_134176/file_handling3.java
+++ src/week12_134176/file_handling3.java
@@ -1,5 +1,22 @@
 package week12_134176;
 
-public class file_handling3 {
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class file_handling3 {
+    public static void main(String[] args) {
+        try {
+            FileWriter writer = new FileWriter("myfile.txt");
+
+            writer.write("Nama saya John Doe\n");
+            writer.write("Saya dari Universitas Multimedia Nusantara");
+
+            writer.close();
+            System.out.println("Successfully written.");
+
+        } catch (IOException e) {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }
+    }
 }
