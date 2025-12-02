50b02837eac33212bb60ec0643a1b8f7ab36bdb4
Grace <aquijade06@gmailcom>
2025-12-02 22:32:22 +0700

    Write File myfile.txt

--- src/week12_134176/file_handling.java
+++ src/week12_134176/file_handling.java
@@ -1,5 +1,22 @@
 package week12_134176;
 
-public class file_handling {
+import java.io.File;
+import java.io.IOException;
 
+public class file_handling {
+    public static void main(String[] args) {
+        try {
+            File obj = new File("myfile.txt");
+
+            if (obj.createNewFile()) {
+                System.out.println("File created: " + obj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+
+        } catch (IOException e) {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }
+    }
 }
